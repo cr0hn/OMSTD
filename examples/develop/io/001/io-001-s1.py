49db14d4aa2bb24076fbb5ad3c7ee573dfb37c3c
pertxas <pertxas@users.noreply.github.com>
2014-12-02 23:03:15 +0100

    Update io-001-s1.py

--- examples/develop/io/001/io-001-s1.py
+++ examples/develop/io/001/io-001-s1.py
@@ -30,6 +30,7 @@ class Displayer:
     def __new__(cls, *args, **kwargs):
         if cls.instance is None:
             cls.instance = object.__new__(cls, *args, **kwargs)
+        return cls.instance
 
     def config(self, **kwargs):
         self.out_file = kwargs.get("out_file", None)
