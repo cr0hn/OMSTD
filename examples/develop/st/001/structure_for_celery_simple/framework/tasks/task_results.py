4055a88104c71f3eb54fc304e55016e62bd07cf2
cr0hn <cr0hn@cr0hn.com>
2014-12-06 09:07:11 +0100

    Added lib/ files. Excluded previously for .gitignore fail

---
+++ examples/develop/st/001/structure_for_celery_simple/framework/tasks/task_results.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+Project name: Open Methodology for Security Tool Developers
+Project URL: https://github.com/cr0hn/OMSTD
+
+Copyright (c) 2014, cr0hn<-AT->cr0hn.com
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+
+import csv
+
+from lib.data import Results, Parameters
+from framework.celery.celery import celery
+
+
+# ----------------------------------------------------------------------
+@celery.task
+def process_results(input_params, results):
+    """
+    YOUR COMMENT FOR TASK 2 HERE
+
+    :param input_params: Parameters object with global input parameters
+    :type input_params: Parameters
+    
+    :param results: Results object
+    :type results: Results
+    
+    """
+
+    # TODO
+    #
+    # YOUR CODE FOR RESULTS PROCESS
+    #
+    #
